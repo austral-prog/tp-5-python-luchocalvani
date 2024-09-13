51b6507d568752c395aad8366ec06c5ec0f019c5
luchocalvani <luchocalvani@gmail.com>
2024-09-12 21:47:06 -0300

    entrega

--- src/Book.py
+++ src/Book.py
@@ -2,39 +2,40 @@
 
 
 class Book:
-    def __init__(self, isbn, title, author, available=True, checkout_num=0):
-        self.__isbn = isbn
-        self.__title = title
-        self.__author = author
-        self.__available = available
-        self.__checkout_num = checkout_num
+    def __init__(self, isbn: str, title: str, author: str, available: bool=True, checkout_num: int=0):
+        self.__isbn: str = isbn
+        self.__title: str = title
+        self.__author: str = author
+        self.__available: bool = available
+        self.__checkout_num: int = checkout_num
 
     # Getters
-    def get_isbn(self):
+    def get_isbn(self) -> str:
         return self.__isbn
 
-    def get_title(self):
+    def get_title(self) -> str:
         return self.__title
 
-    def get_author(self):
+    def get_author(self) -> str:
         return self.__author
 
-    def is_available(self):
+    def is_available(self) -> bool:
         return self.__available
 
-    def get_checkout_num(self):
+    def get_checkout_num(self) -> int:
         return self.__checkout_num
 
     # Setters
-    def set_available(self, available):
-        pass
+    def set_available(self, available: bool) -> None:
+        self.__available = available
 
-    def increment_checkout_num(self):
-        pass
+    def increment_checkout_num(self) -> None:
+        self.__checkout_num += 1
 
     # Utils
     def __str__(self) -> str:
-        pass
+        return f"ISBN: {self.get_isbn()}, Title: {self.get_title()}, Author: {self.get_author()}"
 
-    def __eq__(self, other):
-        pass
+    def __eq__(self, other: 'Book') -> bool:
+        return self.__isbn == other.get_isbn()
+
--- src/Library.py
+++ src/Library.py
@@ -4,40 +4,75 @@ from src.User import User
 
 class Library:
     def __init__(self):
-        self.__books = []
-        self.__users = []
-        self.__checked_out_books = []
-        self.__checked_in_books = []
+        self.__books: list[Book] = []
+        self.__users: list[User] = []
+        self.__checked_out_books: list[list[str]] = []
+        self.__checked_in_books: list[list[str]] = []
 
     # Getters
-    def get_books(self):
+    def get_books(self) -> list[Book]:
         return self.__books
 
-    def get_users(self):
+    def get_users(self) -> list[User]:
         return self.__users
 
-    def get_checked_out_books(self):
+    def get_checked_out_books(self) -> list[list[str]]:
         return self.__checked_out_books
 
-    def get_checked_in_books(self):
+    def get_checked_in_books(self) -> list[list[str]]:
         return self.__checked_in_books
 
     # 1.1 Add Book
-    def add_book(self, isbn, title, author):
-        pass
+    def add_book(self, isbn: str, title: str, author: str) -> None:
+        libro = Book(isbn, title, author)
+        for book in self.__books:
+            if book.get_isbn() == isbn:
+                return None
+        self.__books.append(libro)
 
     # 1.2 List All Books
-    def list_all_books(self):
-        pass
+    def list_all_books(self) -> None:
+        for book in self.__books:
+            print(f"ISBN: {book.get_isbn()}, Title: {book.get_title()}, Author: {book.get_author()}")
 
     # 2.1 Check out book
-    def check_out_book(self, isbn, dni, due_date):
-        pass
+    def check_out_book(self, isbn: str, dni: str, due_date: str) -> str:
+        verif1: bool = False
+        verif2: bool = False
+        for user in self.__users:
+            if user.get_dni() == dni:
+                verif1 = True
+        for book in self.__books:
+            if book.get_isbn() == isbn:
+                verif2 = True
+                if book.is_available():
+                    self.__checked_out_books.append([isbn, dni, due_date])
+                    book.set_available(False)
+                    book.increment_checkout_num()
+                    user.increment_checkouts()
+                    return f"User {user} checked out book {isbn}"
+                else:
+                    return f"Book {isbn} is not available"
+        if verif1 == False or verif2 == False:
+            return f"Unable to find the data for the values: ISBN {isbn} and DNI: {dni}"
 
     # 2.2 Check in book
-    def check_in_book(self, isbn, dni, returned_date):
-        pass
+    def check_in_book(self, isbn: str, dni: str, returned_date: str) -> str:
+        for book in self.__books:
+            if book.get_isbn() == isbn:
+                if not book.is_available():
+                    book.set_available(True)
+                    self.__checked_in_books.append([isbn, dni, returned_date])
+                    return f"Book {isbn} checked in by user {dni}"
+                else:
+                    return f"Book {isbn} is not available"
+        return f"Book {isbn} is not available"
+
 
     # Utils
-    def add_user(self, dni, name):
-        pass
+    def add_user(self, dni, name) -> None:
+        new_user = User(dni, name)
+        for user in self.__users:
+            if user.get_dni() == dni:
+                return None
+        self.__users.append(new_user)
